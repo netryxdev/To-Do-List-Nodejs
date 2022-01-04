e51ea3daa8653b35e36511ff8fc39921fdbf19ae
netryxdev <adrianoneto2501@gmail.com>
2022-01-03 23:35:27 -0300

    first commit

---
+++ db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodemvc2', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+try {
+
+    sequelize.authenticate()
+    console.log('Conectamos ao MySQL!')
+} catch (error) {
+    console.log(`Não foi possível conectar: ${error}`)
+}
+
+module.exports = sequelize
---
+++ models/Task.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize')
+
+const db = require('../db/conn')
+
+const Task = db.define('task', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+  },
+  done: {
+    type: DataTypes.BOOLEAN,
+  },
+})
+
+module.exports = Task
